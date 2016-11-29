c7f71814db8388fead696a18e70d62b07ed8fa42
Laurin Brandner <mail@laurinbrandner.ch>
2016-11-29 20:49:58 +0100

    Update to praw4

--- spotit.py
+++ spotit.py
@@ -2,7 +2,6 @@ import sys
 import os
 import re
 import argparse
-import pickle
 from urllib.parse import urlparse
 from enum import Enum
 from pprint import pprint
@@ -23,23 +22,26 @@ def get_token(username):
     return util.prompt_for_user_token(username, "playlist-modify-public")
 
 
-def retrieve_playlist(sp, user_id, playlist_name):
-    offset = 0
-    total = 1
-    while offset < total:
-        response = sp.user_playlists(user_id, offset=offset)
-        playlists = response["items"]
-        playlist = [p for p in playlists if p["name"] == playlist_name]
-        if playlist:
-            return playlist[0]
+def retrieve_playlist(sp, user_id, playlist_id):
+    all_tracks = []
 
-        offset += len(playlists)
-        total = response["total"]
+    results = sp.user_playlist(user_id, playlist_id, fields="tracks,next")
+    tracks = results["tracks"]
+
+    all_tracks += [t["track"]["id"] for t in tracks["items"]]
+
+    while "next" in tracks.keys():
+        tracks = sp.next(tracks)
+        if tracks is None:
+            break
+
+        all_tracks += [t["track"]["id"] for t in tracks["items"]]
+
+    return all_tracks
 
 
 def retrieve_posts(re, subreddit):
-    subreddit = re.get_subreddit(subreddit)
-    posts = subreddit.get_hot()
+    posts = re.subreddit(subreddit).hot(limit=5)
     songs = [p for p in posts if not p.is_self and p.media is not None]
 
     return songs
@@ -78,11 +80,11 @@ def get_track_info(name):
         return (info[0].strip(), info[1].strip())
 
 
-def update_playlist(subreddit, sp_username, re_username, re_client_id, re_client_secret, token, payload):
-    sp = spotipy.Spotify(auth=token)
+def update_playlist(subreddit, re_username, re_client_id, re_client_secret, sp_username, sp_token, sp_playlist_id):
+    sp = spotipy.Spotify(auth=sp_token)
     re = praw.Reddit(user_agent="web:ch.laurinbrandner.spotit:0.0.1 (by /u/" + re_username + ")", client_id=re_client_id, client_secret=re_client_secret)
 
-    playlist = retrieve_playlist(sp, sp_username, "r/" + subreddit)
+    playlist = retrieve_playlist(sp, sp_username, sp_playlist_id)
 
     last_added_song = None
     new_tracks = []
@@ -113,19 +115,10 @@ def update_playlist(subreddit, sp_username, re_username, re_client_id, re_client
                 if len(results) > 0:
                     new_tracks.append(results[0]["id"])
 
-    try:
-        with open(payload, "rb") as file:
-            all_tracks = pickle.load(file)
-    except FileNotFoundError:
-            all_tracks = []
-
-    new_tracks = [t for t in new_tracks if t not in all_tracks]
+    new_tracks = [t for t in new_tracks if t not in playlist]
 
     if len(new_tracks) > 0:
-        with open(payload, "wb") as file:
-            pickle.dump(all_tracks + new_tracks, file)
-
-        sp.user_playlist_add_tracks(sp_username, playlist["id"], new_tracks)
+        sp.user_playlist_add_tracks(sp_username, sp_playlist_id, new_tracks)
 
 
 if __name__ == "__main__":
